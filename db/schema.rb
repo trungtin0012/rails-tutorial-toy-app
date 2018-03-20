531a5611c9f32fc59f8b97d8eac746f7341e36bf
trungtin0012 <trungtinnguyen1994@gmail.com>
2018-03-20 09:52:24 +0700

    User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312064620) do
+ActiveRecord::Schema.define(version: 20180319105858) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180312064620) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
